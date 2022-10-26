4ab68f8d9832417c04fede7d6307be1878b3f660
Sourav <souravkhare34@gmail.com>
2022-10-26 08:51:23 +0530

    Program to show player is at Poaition 0

---
+++ SnakeAndLadderUC1.java
@@ -0,0 +1,12 @@
+package review.snakeandladder;
+
+public class SnakeAndLadderUC1 {
+    public static void main(String[] args) {
+        int position =0;
+
+        System.out.println("PLayer is at Position :"+position);
+
+    }
+
+
+}
